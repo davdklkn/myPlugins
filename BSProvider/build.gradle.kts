55bea4db14ebc508476290d53633d3334e21db10
davdklkn <davdklkn@gmail.com>
2025-01-28 18:31:29 +0100

    Build e377f54701a0be727f02af00632a56962b81ec42

---
+++ BSProvider/build.gradle.kts
@@ -0,0 +1,24 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Watch Content from Burning Series"
+    authors = listOf("davd")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+
+    tvTypes = listOf("TvSeries")
+
+    language = "de"
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.burning-series.io&sz=%size%"
+}
